f097597b248bf04a3e52cd9f6f7c8c00c05dcfae
glaze <i@inevitable.tech>
2021-06-20 15:59:04 -0700

    Update truffle-config.js

--- templates/truffle-config.js
+++ templates/truffle-config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     compilers: {
         solc: {
-            version: "0.7.6", // ex:  "0.4.20". (Default: Truffle's installed solc)
+            version: "0.8.5", // ex:  "0.4.20". (Default: Truffle's installed solc)
             settings: {
                 optimizer: {
                     enabled: false,
